3db8979fb8b48946408ecea4fc96c9998d5be6db
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-05-13 18:19:52 +0530

    Implemented the header for signin / signup component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Header from "./Header";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Header />
+      <div>
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/ff5587c5-1052-47cf-974b-a97e3b4f0656/065df910-dec3-46ae-afa8-7ad2b52dce40/IN-en-20240506-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="bg-img"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
