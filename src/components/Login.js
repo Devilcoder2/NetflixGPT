77578dd8da00e7767d531921781918cef6cd082e
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-05-15 23:18:16 +0530

    Corrected the regex for email and password validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
 
   const toggleSignInForm = () => {
     setIsSignInForm((prev) => !prev);
   };
 
   const handleButtonClick = () => {
-    //Validate the form data
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
   };
 
   return (
@@ -21,7 +27,10 @@ const Login = () => {
           alt="bg-img"
         />
       </div>
-      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+      >
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -34,15 +43,20 @@ const Login = () => {
           />
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email Address"
           className="p-4 my-4 w-full bg-gray-700"
         />
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-700"
         />
+
+        <p className="text-red-500">{errorMessage}</p>
+
         <button
           className="p-4 my-6 w-full bg-red-700 rounded-lg"
           onClick={handleButtonClick}
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,11 +1,11 @@
 export const checkValidData = (email, password) => {
-  const isEmailValid = /^([a-zA-Z0-9._%-]+\.[a-zA-Z]{2,})$/.test(email);
-
-  const isPasswordValid =
-    "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,10}$".test(
-      password
+  const isEmailValid =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
     );
 
+  const isPasswordValid =
+    /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(password);
   if (!isEmailValid) return "Email ID is not valid";
   if (!isPasswordValid) return "Password is not valid";
 
