ff62c43511d35841a5aa94e5f87ca63c51f9dcac
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-06-01 18:33:19 +0530

    Implemented the Sign out feature

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./Header";
 
 const Browse = () => {
-  return <div>Browse</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Browse;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,36 @@
 import React from "react";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
+
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img
         className="w-44"
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo "
       />
+
+      <div className="flex">
+        <AccountBoxIcon fontSize="large" />
+        <button onClick={handleSignOut} className="font-bold text-white">
+          Sign Out
+        </button>
+      </div>
     </div>
   );
 };
