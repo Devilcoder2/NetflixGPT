7e79cb92247e0062ab20aa00e99cb0c1da831ea0
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-06-01 17:53:49 +0530

    Updating the store on signup/login/logout

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "./Login";
 import Browse from "./Browse";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  useNavigate,
+} from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase.js";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice.js";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -14,6 +25,19 @@ const Body = () => {
       element: <Browse />,
     },
   ]);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, dispalyName } = user;
+        dispatch(addUser({ uid, email, dispalyName }));
+        navigate("/browse");
+      } else {
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+  }, []);
 
   return (
     <div>
